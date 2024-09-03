e3dc98e8227c629872cf910e842f27d3982e0dad
Marco Aurélio Carvalho <65292799+markkcarvalho@users.noreply.github.com>
2024-09-03 17:06:45 -0300

    update

--- aula-202409022100.c
+++ aula-202409022100.c
@@ -1,9 +1,19 @@
 /*
-    Descrição:
-    Autor:
+    Descrição: Entrada e Saída simples de dados
+    Autor: Marco Aurélio
     Data:
     Parâmetros:
 */
 #include <stdio.h>
+#include <stdlib.h>
 
+void main()
+{
+    int idade = 0;
+    char* nome = "";
 
+    printf("Digite seu nome: ");
+    scanf("%s \n", &nome);
+    printf("Digite sua idade: ");
+    scanf("%d \n", &idade);
+}
