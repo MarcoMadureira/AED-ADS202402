13cd2da80ff58447ac3e3cb38c90260ed5e0c208
Marco Aurélio Carvalho <marco.madureira@ifnmg.edu.br>
2024-11-07 21:03:49 -0300

    202411072103
    upd

--- Exemplo de Aula/aula-202411071642.c
+++ Exemplo de Aula/aula-202411071642.c
@@ -13,52 +13,64 @@ typedef struct dados
 } dados;
 
 // Nó que será um elemento da Lista
-typedef struct No
+struct No
 {
     /* data */
     dados Dado;
-    dados * next;
-} No;
-
+    struct No *next;
+};
 
 // Criar uma lista de dados
-No *criarLista(){
-    return NULL;
+struct No *criarLista()
+{
+    struct No * lista = (struct No *)malloc(sizeof(struct No));
+    lista->next = NULL;
+    return lista;
 }
 
-//incluir um elemento na lista criada
-void incluirElementoLista(dados _dados_, No* lista){
-    No *ptTmp = (No*)malloc(sizeof(No));  
+// incluir um elemento na lista criada
+void incluirElementoLista( dados _dados_, struct No *lista)
+{
+    struct No *ptTmp = (struct No *)malloc(sizeof(struct No));
 
     // Se a lista está vazia inclui o elemento
-    if(lista==NULL){
-      lista  = ptTmp;
-      lista->Dado = _dados_;
-      lista->next = NULL;
-      return;      
-    }    
-    else if(lista->next == NULL)
+    if (lista->next == NULL)
     {
-     // insere o novo elemento
-     ptTmp->Dado = _dados_;
-     ptTmp->next = NULL;
-     lista->next = ptTmp;
-     return;
-    } else {
-        // procurar o ultimo elemento da lista antes de incluir
+        lista = ptTmp;
+        lista->Dado = _dados_;
+        lista->next = NULL;
+        return;
     }
-   
 
-   
+    incluirElementoLista(_dados_, ptTmp);
+    return;
+}
 
-// }
+int main()
+{
+    struct No *lstD = criarLista(); // Lista simples
+    dados tmpDados;
 
+    tmpDados.dtNasc = 20241107;
+    tmpDados.endereco = "rua das flores";
+    tmpDados.nome = "Maria das Dores";
+    tmpDados.salario =23000.00;
 
+    incluirElementoLista(tmpDados, lstD);
 
-int main(){
-    No* lstD = criarLista(); // Lista simples
-    
-    printf("Lista %p", lstD);
-    printf(" lista Proximo %p", lstD->next);
+    printf("Ola");
+    printf("Lista %p\n", lstD);
+    //printf(" lista Proximo %p", (lstD==NULL? 0: lstD->next));
+
+    //incluirElementoLista(tmpDados, lstD);
+
+    printf("Lista %p, proximo %p \n", *lstD, lstD->next );
+
+    incluirElementoLista(tmpDados, lstD);
+
+    printf("Ola");
+    printf("Lista %p\n", lstD);
+
+    printf("Lista %p, proximo %p \n", *lstD, &lstD->next );
 
 }
